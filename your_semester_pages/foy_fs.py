6f296c41d8eb90d4e275c263ffa55932470c407d
Dennis Amematekpor Anane <dennisamematekpor@gmail.com>
2025-07-07 21:11:53 +0200

    completed courses

--- your_semester_pages/foy_fs.py
+++ your_semester_pages/foy_fs.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import pandas as pd 
 
 def render():
     st.title("Fourth Year - First Semester 7/8")
     st.write("Welcome to your first semester of fourth year!")
-    # Add your custom logic here
+
+    course_summary = pd.DataFrame({
+        "Course Code": ["CSM 477", "CSM 481", "CSM 483", "CSM 491", "CSM 495", "CSM 497", "MAS 261"],
+        "Course Name": ["DATA COMMUNICATIONS",
+                        "INFORMATION SYSTEMS I",
+                        "OPERATING SYSTEMS ",
+                        "GRAPH THEORY AND ITS APPLICATIONS ",
+                        "INTRODUCTION TO SOFTWARE ENGINEERING",
+                        "EXPERT SYSTEMS",
+                        "PRINCIPLES OF MANAGEMENT"],
+        "Credits": ["3", "3", "3", "2", "2", "2", "3"]
+    })
+
+    st.table(course_summary)
--- your_semester_pages/foy_ss.py
+++ your_semester_pages/foy_ss.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import pandas as pd 
 
 def render():
     st.title("Fourth Year - Second Semester 8/8")
     st.write("Welcome to your last semester as a KNUST Computer Science Student 🔥!")
-    # Add your custom logic here
+
+    course_summary = pd.DataFrame({
+        "Course Code": ["CSM 478", "CSM 482", "CSM 494", "MGT 472", "CSM 490", "CSM 484"],
+        "Course Name": ["COMPUTER NETWORKS",
+                        "INFORMATION SYSTEMS II",
+                        "COMPUTER SECURITY",
+                        "PRINCIPLES OF MANAGEMENT II",
+                        "PROJECT II",
+                        "INTRODUCTION TO COMPILERS"],
+        "Credits": ["3", "3", "3", "3", "6", "3"]
+    })
+
+    st.table(course_summary)
