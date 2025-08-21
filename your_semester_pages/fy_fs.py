db9cf4022d2b51a6f19755ce10da17947bada266
Dennis Amematekpor Anane <dennisamematekpor@gmail.com>
2025-08-21 10:34:54 -0400

    check

--- your_semester_pages/fy_fs.py
+++ your_semester_pages/fy_fs.py
@@ -4,7 +4,6 @@ import pandas as pd
 def render():
     st.title("First Year - First Semester 1/8")
     st.write("Welcome to your first semester of first year!")
-    # Add your custom logic here
 
     course_summary = pd.DataFrame({
         "Course Code": ["CSM 151", "CSM 157", "CSM 165", "CSM 153", "ECON 151", "ENGL 157", "MATH 161"],
